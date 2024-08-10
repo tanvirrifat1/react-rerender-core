1e58f0ebd8b8f50619ce4aeb376a631f0c834f05
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-10 08:00:39 +0800

    added

--- src/pages/UseStateExam.tsx
+++ src/pages/UseStateExam.tsx
@@ -4,10 +4,17 @@ const UseStateExam = () => {
   // const [name, setName] = useState("");
   // const [email, setEmail] = useState("");
 
-  const [user, setUser] = useState({ name: "", email: "", password: "" });
+  const [user, setUser] = useState({ name: "", email: "" });
 
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = e.target.name;
+    const value = e.target.value;
+
+    setUser({ ...user, [inputName]: value });
     console.log(user);
   };
 
@@ -16,24 +23,17 @@ const UseStateExam = () => {
       <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-2">
           <input
-            onChange={(e) => setUser({ ...user, name: e.target.value })}
+            onChange={handleChange}
             type="text"
             name="name"
             id="name"
             className="border border-black text-black"
           />
           <input
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            onChange={handleChange}
             type="email"
             name="email"
             id="email"
-            className="border border-black text-black"
-          />
-          <input
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-            type="password"
-            name="password"
-            id="password"
             className="border border-black text-black"
           />
         </div>
