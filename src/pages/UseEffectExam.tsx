120902abe25f09d09f87b8f297b329a4f179db90
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-08-12 23:40:03 +0800

    added

--- src/pages/UseEffectExam.tsx
+++ src/pages/UseEffectExam.tsx
@@ -3,15 +3,40 @@ import React, { useEffect, useState } from "react";
 const UseEffectExam = () => {
   const [hidden, setHidden] = useState(false);
 
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  useEffect(() => {
+    console.log("render");
+  }, [user]);
+
   return (
     <div>
-      <button
+      {/* <button
         onClick={() => setHidden((prev) => !prev)}
         className="btn btn-outline my-5"
       >
         {hidden ? "show" : "hide"}
       </button>
-      {!hidden && <Counter />}
+      {!hidden && <Todo />} */}
+      <input
+        onBlur={(e) => setUser({ ...user, name: e.target.value })}
+        type="text"
+        name="name"
+        id="name"
+        className="border border-primary"
+      />
+      <input
+        onBlur={(e) => setUser({ ...user, email: e.target.value })}
+        type="text"
+        name="name"
+        id="name"
+        className="border border-primary"
+      />
+      <div className="flex justify-center mt-6">
+        <button type="submit" className="btn btn-secondary">
+          submit
+        </button>
+      </div>
     </div>
   );
 };
@@ -30,6 +55,21 @@ const Counter = () => {
   }, []);
 
   return <h1 className="border border-yellow-800 p-8">{count}</h1>;
+};
+
+const Todo = () => {
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1", { signal })
+      .then((response) => response.json())
+      .then((data) => alert(data.title));
+    return () => {
+      controller.abort();
+    };
+  }, []);
+  return <h1 className="border border-primary p-10"></h1>;
 };
 
 export default UseEffectExam;
